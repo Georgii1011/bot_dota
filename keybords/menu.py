9cac978b0bdf969b88bd879e59a68e6f4ec18c9c
Georgii1011 <145004542+Georgii1011@users.noreply.github.com>
2025-06-22 13:30:30 +0300

    Добавил всех героев для работы с командами из меню героев

--- keybords/menu.py
+++ keybords/menu.py
@@ -126,12 +126,112 @@ def get_numbers_menu():
 def get_listofheroes_menu():
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
     kb.row(
-        KeyboardButton("Ogre Magi"),
-        KeyboardButton("Ogre Magi"),
-        KeyboardButton("Ogre Magi"),
-        KeyboardButton("Ogre Magi"),
-        KeyboardButton("Ogre Magi"),
-        KeyboardButton("Ogre Magi"),
+        KeyboardButton("Abaddon"), KeyboardButton("Alchemist"),
+        KeyboardButton("Ancient Apparition"), KeyboardButton("Anti-Mage"),
+        KeyboardButton("Arc Warden"), KeyboardButton("Axe")
+    )
+    kb.row(
+        KeyboardButton("Bane"), KeyboardButton("Batrider"),
+        KeyboardButton("Beastmaster"), KeyboardButton("Bloodseeker"),
+        KeyboardButton("Bounty Hunter"), KeyboardButton("Brewmaster")
+    )
+    kb.row(
+        KeyboardButton("Bristleback"), KeyboardButton("Broodmother"),
+        KeyboardButton("Centaur Warrunner"), KeyboardButton("Chaos Knight"),
+        KeyboardButton("Chen"), KeyboardButton("Clinkz")
+    )
+    kb.row(
+        KeyboardButton("Clockwerk"), KeyboardButton("Crystal Maiden"),
+        KeyboardButton("Dark Seer"), KeyboardButton("Dark Willow"),
+        KeyboardButton("Dawnbreaker"), KeyboardButton("Dazzle")
+    )
+    kb.row(
+        KeyboardButton("Death Prophet"), KeyboardButton("Disruptor"),
+        KeyboardButton("Doom"), KeyboardButton("Dragon Knight"),
+        KeyboardButton("Drow Ranger"), KeyboardButton("Earth Spirit")
+    )
+    kb.row(
+        KeyboardButton("Earthshaker"), KeyboardButton("Elder Titan"),
+        KeyboardButton("Ember Spirit"), KeyboardButton("Enchantress"),
+        KeyboardButton("Enigma"), KeyboardButton("Faceless Void")
+    )
+    kb.row(
+        KeyboardButton("Grimstroke"), KeyboardButton("Gyrocopter"),
+        KeyboardButton("Hoodwink"), KeyboardButton("Huskar"),
+        KeyboardButton("Invoker"), KeyboardButton("Io")
+    )
+    kb.row(
+        KeyboardButton("Jakiro"), KeyboardButton("Juggernaut"),
+        KeyboardButton("Keeper of the Light"), KeyboardButton("Kez"),
+        KeyboardButton("Kunkka"), KeyboardButton("Legion Commander")
+    )
+    kb.row(
+        KeyboardButton("Leshrac"), KeyboardButton("Lich"),
+        KeyboardButton("Lifestealer"), KeyboardButton("Lina"),
+        KeyboardButton("Lion"), KeyboardButton("Lone Druid")
+    )
+    kb.row(
+        KeyboardButton("Luna"), KeyboardButton("Lycan"),
+        KeyboardButton("Magnus"),KeyboardButton("Marci"),
+        KeyboardButton("Mars"), KeyboardButton("Medusa")
+    )
+    kb.row(
+        KeyboardButton("Meepo"), KeyboardButton("Mirana"),
+        KeyboardButton("Monkey King"), KeyboardButton("Morphling"),
+        KeyboardButton("Muerta"), KeyboardButton("Naga Siren")
+    )
+    kb.row(
+        KeyboardButton("Nature's Prophet"), KeyboardButton("Necrophos"),
+        KeyboardButton("Night Stalker"), KeyboardButton("Nyx Assassin"),
+        KeyboardButton("Ogre Magi"), KeyboardButton("Omniknight")
+    )
+    kb.row(
+        KeyboardButton("Oracle"), KeyboardButton("Outworld Destroyer"),
+        KeyboardButton("Pangolier"), KeyboardButton("Phantom Assassin"),
+        KeyboardButton("Phantom Lancer"), KeyboardButton("Phoenix")
+    )
+    kb.row(
+        KeyboardButton("Primal Beast"), KeyboardButton("Puck"),
+        KeyboardButton("Pudge"), KeyboardButton("Pugna"),
+        KeyboardButton("Queen of Pain"), KeyboardButton("Razor")
+    )
+    kb.row(
+        KeyboardButton("Riki"), KeyboardButton("Ringmaster"),
+        KeyboardButton("Rubick"), KeyboardButton("Sand King"),
+        KeyboardButton("Shadow Demon"), KeyboardButton("Shadow Fiend"),
+    )
+    kb.row(
+        KeyboardButton("Shadow Shaman"), KeyboardButton("Silencer"),
+        KeyboardButton("Skywrath Mage"), KeyboardButton("Slardar"),
+        KeyboardButton("Slark"), KeyboardButton("Snapfire"),
+
+    )
+    kb.row(
+        KeyboardButton("Sniper"), KeyboardButton("Spectre"),
+        KeyboardButton("Spirit Breaker"), KeyboardButton("Storm Spirit"),
+        KeyboardButton("Sven"), KeyboardButton("Techies"),
+
+    )
+    kb.row(
+        KeyboardButton("Templar Assassin"), KeyboardButton("Terrorblade"),
+        KeyboardButton("Tidehunter"), KeyboardButton("Timbersaw"),
+        KeyboardButton("Tinker"), KeyboardButton("Tiny"),
+    )
+    kb.row(
+        KeyboardButton("Treant Protector"), KeyboardButton("Troll Warlord"),
+        KeyboardButton("Tusk"), KeyboardButton("Underlord"),
+        KeyboardButton("Undying"), KeyboardButton("Ursa"),
+
+    )
+    kb.row(
+        KeyboardButton("Vengeful Spirit"), KeyboardButton("Venomancer"),
+        KeyboardButton("Viper"), KeyboardButton("Visage"),
+        KeyboardButton("Void Spirit"), KeyboardButton("Warlock"),
+    )
+    kb.row(
+        KeyboardButton("Weaver"), KeyboardButton("Windranger"),
+        KeyboardButton("Winter Wyvern"), KeyboardButton("Witch Doctor"),
+        KeyboardButton("Wraith King"), KeyboardButton("Zeus")
     )
     kb.row(
         KeyboardButton("🔙 Назад")
